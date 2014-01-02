6fa9e807019727064bfe3221ea022514b45b9245
Abhijit Tomar <abhijit141088@gmail.com>
2014-01-02 14:52:27 -0500

    Email Thread Summarization
    Two separate approaches, one calculates TF IDF scores using a global
    corpus and the other calculates TF IDF scores locally.

---
+++ Using Global Corpus/AbtJape/src/jape/Jape.java
@@ -0,0 +1,319 @@
+package jape;
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import static gate.Utils.inDocumentOrder;
+import gate.*;
+import static gate.util.persistence.PersistenceManager.loadObjectFromFile;
+
+public class Jape {
+	
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/abtdb";
+    static final String USER = "root";
+  
+	 public static String HOME_PATH = "";
+	 public Jape() throws Exception{
+			Gate.init();
+			File gateHome = Gate.getGateHome();
+			HOME_PATH = gateHome.getCanonicalPath() + "/";
+			
+			//System.out.println("Home path: "+ HOME_PATH);
+			if (Gate.getGateHome() == null)
+				Gate.setGateHome(gateHome);
+			
+			
+			File pluginsHome = new File(gateHome, "plugins");
+			//Register all the plugins that your program will need
+			Gate.getCreoleRegister().registerDirectories(
+					new File(pluginsHome, "ANNIE")
+							.toURI().toURL());
+	 }
+	 
+	 @SuppressWarnings("unchecked")
+	public static void main(String[] ars)throws Exception
+	 {
+		 
+		new Jape();
+		//Point it to where your gapp file resides on your hard drive
+		 gate.CorpusController c = ((gate.CorpusController)loadObjectFromFile(new java.io.File("C:/Program Files (x86)/GATE_Developer_7.1/A/1/emailsumm.gapp")));
+			Corpus corpus = (Corpus) Factory.createResource("gate.corpora.CorpusImpl");
+			//Point it to whichever folder contains your documents
+			URL dir = new File("C:/Program Files (x86)/GATE_Developer_7.1/A/1/Text").toURI().toURL();
+			corpus.populate(dir, null, "UTF-8", false); //set the encoding to whatever is the encoding of your files
+			
+			c.setCorpus(corpus);
+			c.execute();
+			
+			HashMap<String, Integer> firstnouns = new HashMap<String, Integer>();// HashMap for storing counts of nouns in the first email
+			
+			Set<String> logins = new HashSet<String>();
+			
+			ArrayList<String> logs = new ArrayList<String>(); // These two would be used for extracting and storing all the email id's in this thread
+			
+			HashMap<String, Integer> nouncounter = new HashMap<String, Integer>();// HashMap for storing the counts of all the nouns
+			
+			List<Annotation> lis = null; // This list would be used to extract all the NVN annotations in the sequence that they appear in the original email
+			
+			gate.Document tempDoc = null;
+			
+			int paracount=0;		// We shall keep a count of how many NVN annotations we receive. The we shall use this value to extract the top nouns
+			
+			//This is how you can access the annotations created by your gate application
+			for (int i = 0; i < corpus.size(); i++)
+			{
+				int b=0;
+				
+				tempDoc = (gate.Document)corpus.get(i);
+				
+				for(gate.Annotation ar: tempDoc.getAnnotations().get("EmailID")) // Get all the email ids of the thread, annotated as EmailID
+				{
+					
+					logs =  (ArrayList<String>) ar.getFeatures().get("Login");	// Extract the feature value Login.
+					
+						for(String l : logs)
+							logins.add(l);
+			
+				}
+				
+				for(gate.Annotation ar: tempDoc.getAnnotations().get("Email1")) // Get the first email of the thread, annotated as Email1
+				{
+					firstnouns=(HashMap<String, Integer>) ar.getFeatures().get("NounMap");//Store all the nouns in a HashMap
+			
+				}
+				
+				lis = inDocumentOrder(tempDoc.getAnnotations().get("Valid"));		// Extract all the NVN annotations in sequence
+				
+				for(gate.Annotation a: lis) 
+				{
+					paracount++;
+					HashMap<String, Integer> nounkey = new HashMap<String, Integer>();//This list will store all the nouns associated with a NVN annotation
+					
+					try
+					{
+						
+					nounkey=(HashMap<String, Integer>) a.getFeatures().get("Nouns");// Get all the nouns associated with this NVN annotation
+					
+					for (Map.Entry<String, Integer> entry : nounkey.entrySet())// Iterate over all the nouns in this NVN annotation
+					{
+						String nk = entry.getKey();
+						int tf = entry.getValue();
+						
+						if(!nk.isEmpty() && !logins.contains(nk))// If the noun obtained is not a blank string and it is not an email ID then proceed
+						{
+							
+						if(nouncounter.containsKey(nk))// If this noun already appears in our nouncounter HashMap, then increase its count by 1
+							nouncounter.put(nk, nouncounter.get(nk)+tf);
+						else// If this noun does not appear in our nouncounter HashMap, meaning this is the first time we see it, set its count to 1
+							nouncounter.put(nk, tf);
+						
+						}
+					
+					}
+					
+					
+					}
+					catch(NullPointerException e)
+					{
+						
+						
+					}
+				
+			}
+				//Really important to release these resources or you'll soon run out of working memory
+				Factory.deleteResource(tempDoc);
+			   
+			}
+			
+			 Factory.deleteResource(corpus);
+			
+			ArrayList<String> nn = new ArrayList<String>();// This list represents the high scoring nouns that we will select
+			
+			nn = Jape.Scores(nouncounter, firstnouns, paracount);//Get the high scoring nouns from the function Scores. Pass the nouncounter, 
+		
+			ArrayList<String> nvn = new ArrayList<String>();
+			
+			for(gate.Annotation a: lis) 	//Now iterate over the NVN annotations again, this time collecting the string values of the top nouns that we got above
+			{
+				HashMap<String, Integer> finalnounkey = (HashMap<String, Integer>) a.getFeatures().get("Nouns");	
+				
+				for(Map.Entry<String, Integer> entry : finalnounkey.entrySet())
+				{
+					String n = entry.getKey();
+					
+					if(nn.contains(n))
+						if(!nvn.contains(a.getFeatures().get("Value").toString()))	// Add this string to the final summary only if it has not been seen before
+								nvn.add(a.getFeatures().get("Value").toString());
+				}
+			}
+			
+			for(String jb : nvn)
+			{			
+				System.out.println(jb);	// Print out the summary
+			}
+			
+	 }
+
+	public static ArrayList<String> Scores(HashMap<String, Integer> nouncounter, HashMap<String, Integer> firstnouns, double paracount) 
+	{
+		 Connection c = null;
+         PreparedStatement pstmt=null;
+         
+         Double globalcount =  (paracount/10.0);	// This count represents the number of the nouns which will be our candidates from the sorted HashMap
+        	 
+         HashMap<String, Double> nounscore = new HashMap<String, Double>();// HashMap for storing the tfidf scores of the nouns in the nouncounter
+
+         for(Map.Entry <String, Integer> entry : nouncounter.entrySet())//Iterate over all the nouns in the nouncounter
+         try
+         {
+        	 String term = entry.getKey();//Get the noun
+        	 
+        	 int df=1;//df is set to one by default 
+        			 
+        	 int tf=entry.getValue();// Get the count associated with this noun. This will be the tf
+	         
+        	 double tfidf;
+	         
+        	 Class.forName("com.mysql.jdbc.Driver");
+        	 c = DriverManager.getConnection(DB_URL,USER,"");
+        	 
+        	 pstmt = c.prepareStatement("select DocumentFreq from JapeTab where Term=?");//Get the value of the idf for this noun from the database
+	         pstmt.setString(1, term);
+	         
+	         ResultSet rs = pstmt.executeQuery();
+	         
+	        
+	         if(rs.next())// If a row exists in the database for this noun, then retrieve it
+	         {
+	        	 df=rs.getInt("DocumentFreq");//df holds the value of the document frequency of this noun
+	        	
+	        	 tfidf=tf*Math.log10(88/df);//Calculate the tfidf score for this noun
+	        	
+	        	 if(firstnouns.containsKey(term))// If this noun also appears in firstnouns then assign it a higher score. 
+	        		 															//Store the final tfidf score in 
+	        		 							// nounscores
+	        		 nounscore.put(term, tfidf+1);
+	        	 
+	        	else//If not then, store the tfidf score in nounscores
+	        		 nounscore.put(term, tfidf);
+	        		 
+	         }
+	         
+	         else//If no row exists in the database for this noun, then it is the first time ever we are seeing this noun. Hence its df = 1
+	         
+	        	 {
+	        	 		if(term.matches("[\\p{Alnum}]+"))//Eliminate non alphanumeric nouns
+	        	 		{
+	        	 			
+	        	 			tfidf=tf*Math.log10(88/df);
+	        	 			nounscore.put(term, tfidf);
+		        	 
+	        	 		}
+		         }
+		        
+	         
+	         pstmt.close();
+             c.close();
+            
+        }
+        
+       catch (SQLException se)
+        {
+            se.printStackTrace();
+        }
+        
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        finally 
+        {
+            try
+            {
+                if(pstmt!=null)
+                    pstmt.close();
+                
+            }
+            
+            catch (SQLException se2)
+            {}
+            
+        }
+        
+        try 
+        {
+            if(c!=null)
+                c.close();
+        }
+        
+        catch(SQLException se)
+        {
+            se.printStackTrace();
+        }
+        
+        ArrayList<String> its = new ArrayList<String>();// This is the list which is returned to the main function. It will hold all the high scoring nouns
+      
+        // We need to sort the nounscore HashMap by value
+        ValueComparator bvc =  new ValueComparator(nounscore);
+        
+        TreeMap<String,Double> sorted_map = new TreeMap<String,Double>(bvc);
+        int count = 0;
+        sorted_map.putAll(nounscore);// sorted_map now has the nounscore HashMap sorted by value in descending order
+        System.out.println(globalcount);
+        for ( Map.Entry<String, Double> entry : sorted_map.entrySet()) 
+		 {System.out.println(entry.getKey()+" "+entry.getValue());
+        	if(count<4/*entry.getValue()>globalcount*/)//Here we will extract the top values and store them in the ArrayList which would be returned 
+			{
+        		count++;
+				its.add(entry.getKey());// Add the entry to the list its
+			}
+		 }
+		
+		return its;
+	}
+	
+	
+}
+
+// Sort in descending order
+class ValueComparator implements Comparator<String> {
+
+	   Map<String, Double> base;
+	   public ValueComparator(Map<String, Double> base) {
+	       this.base = base;
+	   }
+
+	   // Note: this comparator imposes orderings that are inconsistent with equals.    
+	   public int compare(String a, String b) {
+	       if (base.get(a) >= base.get(b)) {
+	           return -1;
+	       } else {
+	           return 1;
+	       } // returning 0 would merge keys
+	   }
+	}
+
+//Sort in ascending order
+class ValueComparatorAsc implements Comparator<String> {
+
+	   Map<String, Integer> base;
+	   public ValueComparatorAsc(Map<String, Integer> base) {
+	       this.base = base;
+	   }
+
+	   // Note: this comparator imposes orderings that are inconsistent with equals.    
+	   public int compare(String a, String b) {
+	       if (base.get(a) <= base.get(b)) {
+	           return -1;
+	       } else {
+	           return 1;
+	       } // returning 0 would merge keys
+	   }
+	}
+	 
